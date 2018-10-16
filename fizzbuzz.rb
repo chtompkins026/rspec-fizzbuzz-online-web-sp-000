b8010c41f123820572b163996a089eadc569a5fd
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-16 15:19:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-
   if num % 5 == 0 && num % 3 == 0 
     return "FizzBuzz"
   elsif num % 3 == 0 && num % 5 != 0 
-    return 
+    return "Fizz" 
+  elsif num % 5 == 0 && num % 3 !=0 
+    return "Buzz" 
+  else 
+    continue 
+  end 
+  
 end 
